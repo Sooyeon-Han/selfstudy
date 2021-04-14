44344e61f509b67bda6db735d1da167c0c7857d2
Sooyeon-Han <h.soooyeon@gmail.com>
2021-04-14 23:52:41 +0900

    0416) JAVA의 정석 CHAPTER 7. 객체지향 프로그래밍 2

---
+++ src/java_bible/ch07/ex26.java
@@ -0,0 +1,74 @@
+package java_bible.ch07;
+
+public class ex26 {
+	public static void main(String[] args) {
+		Tank tank = new Tank();
+		Dropship dropship = new Dropship();
+		Marine marine = new Marine();
+		
+		SCV scv = new SCV();
+		scv.repair(tank);
+		scv.repair(dropship);
+//		scv.repair(marine);	에러발생
+	}
+}
+
+interface Repairable{}
+
+class Units{
+	int hitPoint;
+	final int MAX_HP;
+	Units(int hp){
+		MAX_HP = hp;
+	}
+}
+
+class GroundUnit extends Units{
+	GroundUnit(int hp){
+		super(hp);
+	}
+}
+
+class AirUnit extends Units{
+	AirUnit(int hp){
+		super(hp);
+	}
+}
+
+class Tank extends GroundUnit implements Repairable{
+	Tank(){
+		super(150);
+		hitPoint = MAX_HP;
+	}
+}
+
+class Dropship extends AirUnit implements Repairable{
+	Dropship(){
+		super(125);
+		hitPoint = MAX_HP;
+	}
+}
+
+class Marine extends GroundUnit{
+	Marine(){
+		super(40);
+		hitPoint = MAX_HP;
+	}
+}
+
+class SCV extends GroundUnit implements Repairable{
+	SCV(){
+		super(60);
+		hitPoint = MAX_HP;		
+	}
+	
+	void repair(Repairable r) {
+		if(r instanceof Units) {
+			Units u = (Units)r;
+			while(u.hitPoint!=u.MAX_HP) {
+				u.hitPoint++;
+			}
+			System.out.println(u.toString()+"의 수리가 끝났습니다.");
+		}
+	}
+}
