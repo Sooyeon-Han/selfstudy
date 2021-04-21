682d999f049f2ec67120dad009409aea9d7c9a6d
Sooyeon-Han <h.soooyeon@gmail.com>
2021-04-21 23:01:11 +0900

    0421) JAVA의 정석 CHAPTER 7. 객체지향 프로그래밍 2

---
+++ src/java_bible/ch07/ex37.java
@@ -0,0 +1,18 @@
+package java_bible.ch07;
+
+import java.awt.Button;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ex37 {
+	public static void main(String[] args) {
+		Button b = new Button("Start");
+		b.addActionListener(new EventHandler());
+	}
+}	
+
+class EventHandler implements ActionListener{
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("ActionEvent Occurred!");
+	}
+}
