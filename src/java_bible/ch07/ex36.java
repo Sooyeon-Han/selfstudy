f1e9bff93b16db8a6c8022426c3de1b11f493363
Sooyeon-Han <h.soooyeon@gmail.com>
2021-04-21 23:00:31 +0900

    0421) JAVA의 정석 CHAPTER 7. 객체지향 프로그래밍 2

---
+++ src/java_bible/ch07/ex36.java
@@ -0,0 +1,10 @@
+package java_bible.ch07;
+
+public class ex36 {
+	Object iv = new Object() {	void method() {}};		//익명 클래스
+	static Object cv = new Object() {void method(){}};	//익명 클래스
+	
+	void myMethod() {
+		Object lv = new Object() {void method(){}};
+	}
+}
