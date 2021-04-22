93bbd9e727e5babb86637793b0ab85b95491de1f
Sooyeon-Han <h.soooyeon@gmail.com>
2021-04-22 23:25:45 +0900

    0422) JAVA의 정석 CHAPTER 8. 예외처리

---
+++ src/java_bible/ch08/ex01.java
@@ -0,0 +1,26 @@
+package java_bible.ch08;
+
+public class ex01 {
+	public static void main(String[] args) {
+		try {
+			try {
+				
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			try {
+				
+			} catch (Exception e2) {	//e로 하면 에러 발생
+				// TODO: handle exception
+			}
+		}	//try-catch 끝
+		
+		try {
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}	//try-catch 끝
+	}		//main 메서드 끝
+}
