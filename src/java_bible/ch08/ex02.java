9876907acda351078dd44c5719bd68520b42ccc8
Sooyeon-Han <h.soooyeon@gmail.com>
2021-04-22 23:26:04 +0900

    0422) JAVA의 정석 CHAPTER 8. 예외처리

---
+++ src/java_bible/ch08/ex02.java
@@ -0,0 +1,13 @@
+package java_bible.ch08;
+
+public class ex02 {
+	public static void main(String[] args) {
+		int number = 10;
+		int result = 0;
+		
+		for(int i=0; i<10; i++) {
+			result = number / (int)(Math.random()*10);
+			System.out.println(result);
+		}
+	}
+}
