e2cfc94b772693dd5db80a58a36adf629b8ae424
Sooyeon-Han <h.soooyeon@gmail.com>
2021-04-23 23:39:13 +0900

    0423) JAVA의 정석 CHAPTER 8. 예외처리

---
+++ src/java_bible/ch08/ex04.java
@@ -0,0 +1,16 @@
+package java_bible.ch08;
+
+public class ex04 {
+	public static void main(String[] args) {
+		System.out.println(1);
+		System.out.println(2);
+		try {
+			System.out.println(3);
+			System.out.println(4);
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(5);
+		}
+		System.out.println(6);
+	}
+}
