10852c73ee84d9120e2df184a02c1047a6aacc4c
Sooyeon-Han <h.soooyeon@gmail.com>
2021-05-03 23:40:16 +0900

    0503) JAVA의 정석 CHAPTER 9. java.lang 패키지와 유용한 클래스

---
+++ src/java_bible/ch09/ex06.java
@@ -0,0 +1,11 @@
+package java_bible.ch09;
+
+public class ex06 {
+	public static void main(String[] args) {
+		Card c1 = new Card();
+		Card c2 = new Card("HEART",10);
+		
+		System.out.println(c1.toString());
+		System.out.println(c2.toString());
+	}
+}
