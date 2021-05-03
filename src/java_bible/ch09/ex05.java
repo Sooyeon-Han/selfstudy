9e2afed914b9d05e14ade3c0a9e6c9212159ede5
Sooyeon-Han <h.soooyeon@gmail.com>
2021-05-03 23:40:12 +0900

    0503) JAVA의 정석 CHAPTER 9. java.lang 패키지와 유용한 클래스

---
+++ src/java_bible/ch09/ex05.java
@@ -0,0 +1,13 @@
+package java_bible.ch09;
+
+public class ex05 {
+	public static void main(String[] args) {
+		String str = new String("KOREA");
+		java.util.Date today = new java.util.Date();
+		
+		System.out.println(str);
+		System.out.println(str.toString());
+		System.out.println(today);
+		System.out.println(today.toString());
+	}
+}
