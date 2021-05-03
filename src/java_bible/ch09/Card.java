f1a8046c4465cecf3c36644627cafd3734b460ce
Sooyeon-Han <h.soooyeon@gmail.com>
2021-05-03 23:40:08 +0900

    0503) JAVA의 정석 CHAPTER 9. java.lang 패키지와 유용한 클래스

---
+++ src/java_bible/ch09/Card.java
@@ -0,0 +1,20 @@
+package java_bible.ch09;
+
+class Card {
+	String kind;
+	int number;
+
+	Card() {
+		this("SPADE", 1); 
+	}
+
+	Card(String kind, int number) {
+		this.kind = kind;
+		this.number = number;
+	}
+
+
+	public String toString() {
+		return "kind : " + kind + ", number : " + number;
+	}
+}
