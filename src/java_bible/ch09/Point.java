b132a384c2b5245f3d480de89b17bb642338660a
Sooyeon-Han <h.soooyeon@gmail.com>
2021-05-04 23:54:43 +0900

    0504) JAVA의 정석 CHAPTER 9. java.lang 패키지와 유용한 클래스

---
+++ src/java_bible/ch09/Point.java
@@ -0,0 +1,24 @@
+package java_bible.ch09;
+
+public class Point implements Cloneable {
+	int x, y;
+	
+	Point(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	public String toString() {
+		return  "x="+x+", y="+y;
+	}
+	
+	public Object clone() {
+		Object obj = null;
+		try {
+			obj = super.clone();	//clone()은 반드시 예외처리를 해주어야한다
+		} catch (CloneNotSupportedException e) {
+			// TODO: handle exception
+			return obj;
+		}
+	}
+}
