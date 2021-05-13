0e3cbc1e9daa74171e488cd9aa344d0cb9f435ec
Sooyeon-Han <h.soooyeon@gmail.com>
2021-05-13 23:56:09 +0900

    0512) JAVA의 정석 CHAPTER 9. java.lang 패키지와 유용한 클래스

--- src/java_bible/ch09/ex20.java
+++ src/java_bible/ch09/ex20.java
@@ -14,6 +14,13 @@ public class ex20 {
 		out.println("round("+val+")/100.0="+round(val)/100.0);
 		out.println();
 		out.printf("ceil(%3.1f)=%3.1f%n", 1.1, ceil(1.1));
-		out.printf("floor(%3.1f)=%3.1f%n", 1.5, ceil(1.5));
+		out.printf("floor(%3.1f)=%3.1f%n", 1.5, floor(1.5));
+		out.printf("round(%3.1f)=%d%n", 1.1, round(1.1));
+		out.printf("round(%3.1f)=%d%n", 1.5, round(1.5));
+		out.printf("rint(%3.1f)=%f%n", 1.5, rint(1.5));
+		out.printf("round(%3.1f)=%d%n", -1.5, round(-1.5));
+		out.printf("rint(%3.1f)=%f%n", -1.5, rint(-1.5));
+		out.printf("ceil(%3.1f)=%f%n", -1.5, ceil(1.5));
+		out.printf("floor(%3.1f)=%f%n", -1.5, floor(1.5));
 	}
 }
