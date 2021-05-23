602d709e0d123edc3d7b14eed7d88dac40c3f5e2
Sooyeon-Han <h.soooyeon@gmail.com>
2021-05-23 23:52:18 +0900

    0523) JAVA의 정석 CHAPTER 9. java.lang 패키지와 유용한 클래스

--- src/java_bible/ch09/ex26.java
+++ src/java_bible/ch09/ex26.java
@@ -1,9 +1,21 @@
 package java_bible.ch09;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class ex26 {
 	public static void main(String[] args) {
 		String [][] str2D = new String [][] {{"aaa","bbb"},{"AAA","BBB"}};
+		String [][] str2D_2 = new String [][] {{"aaa","bbb"},{"AAA","BBB"}};
 		
+		System.out.print("str2D = {");
 		
+		for(String [] tmp : str2D)
+			System.out.print(Arrays.toString(tmp));
+		System.out.print("}");
+		
+		System.out.println("equals(str2D, str2D_2) = )"+Objects.equals(str2D, str2D_2));
+		System.out.println("deepEquals(str2D, str2D_2) = )"+Objects.deepEquals(str2D, str2D_2));
+		//System.out.println("isNull(null) ="+isNull(null));
 	}
 }
