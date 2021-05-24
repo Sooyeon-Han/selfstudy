52ed1ca8b9fbbb4ffdc7514625ff66906a49880e
Sooyeon-Han <h.soooyeon@gmail.com>
2021-05-24 23:18:02 +0900

    0524) JAVA의 정석 CHAPTER 9. java.lang 패키지와 유용한 클래스

--- src/java_bible/ch09/ex26.java
+++ src/java_bible/ch09/ex26.java
@@ -1,7 +1,7 @@
 package java_bible.ch09;
 
-import java.util.Arrays;
-import java.util.Objects;
+import java.util.*;
+import static java.util.Objects.*;
 
 public class ex26 {
 	public static void main(String[] args) {
@@ -16,6 +16,16 @@ public class ex26 {
 		
 		System.out.println("equals(str2D, str2D_2) = )"+Objects.equals(str2D, str2D_2));
 		System.out.println("deepEquals(str2D, str2D_2) = )"+Objects.deepEquals(str2D, str2D_2));
-		//System.out.println("isNull(null) ="+isNull(null));
+		System.out.println("isNull(null) = " + isNull(null));
+		System.out.println("nonNull(null) = "+nonNull(null));
+		System.out.println("hashCode(null) = " + Objects.hashCode(null));
+		System.out.println("toString(null) = "+Objects.toString(null));
+		System.out.println("toString(null, \"\") = "+Objects.toString(null,""));
+		
+		Comparator c = String.CASE_INSENSITIVE_ORDER;
+		
+		System.out.println("compare(\"aa\",\"bb\") = " + compare("aa","bb",c));
+		System.out.println("compare(\"bb\",\"aa\") = " + compare("bb","aa",c));
+		System.out.println("compare(\"ab\",\"AB\") = " + compare("ab","AB",c));
 	}
 }
