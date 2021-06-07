cd4bc936845ac48242db0355cd621215903ba2d7
Sooyeon-Han <h.soooyeon@gmail.com>
2021-06-07 22:40:14 +0900

    0607) JAVA의 정석 CHAPTER 9. java.lang 패키지와 유용한 클래스

--- src/java_bible/ch09/ex13.java
+++ src/java_bible/ch09/ex13.java
@@ -2,6 +2,7 @@ package java_bible.ch09;
 
 public class ex13 {
 	public static void main(String[] args) {
+		
 		//길이가 0인 char 배열 생성
 		char[] cArr = new char[0];		//char[] cArr = ();와 같음
 		String s = new String (cArr);	//String s = new String("");와 같다
