01d754c2459f047f85e08b0d5c8f2ee7556a360d
SYHAN-DESKTOP-H\user <syhan@sphinfo.co.kr>
2021-07-28 17:29:46 +0900

    0728) 생활코딩 Node.js

---
+++ WebContent/Node.js/syntax/array.js
@@ -0,0 +1,13 @@
+//CRUD
+//Array Literal
+
+var arr = ['A', 'B', 'C', 'D'];
+console.log(arr[1]);    //배열의 자릿수는 0부터 카운팅된다.
+console.log(arr[3]); 
+
+arr[2] = 3;
+console.log(arr[2]); 
+console.log(arr.length);
+
+arr.push('E');
+console.log(arr);
